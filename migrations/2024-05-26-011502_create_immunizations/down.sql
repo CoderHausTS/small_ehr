fa5ed78a5b1c4d574de9db6e486698987e9a930a
Jason Woods <jason@coder.haus>
2024-05-27 21:03:43 -0500

    Add immunizations migration

--- migrations/2024-05-26-011502_create_immunizations/down.sql
+++ migrations/2024-05-26-011502_create_immunizations/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DELETE TABLE immunizations
+DROP TABLE immunizations
--- migrations/2024-05-26-011502_create_immunizations/up.sql
+++ migrations/2024-05-26-011502_create_immunizations/up.sql
@@ -1,14 +1,12 @@
 -- Your SQL goes here
-CREATE TABLE immunizations {
-
-	DATE,
-	PATIENT,
-	ENCOUNTER,
-	CODE,
-	DESCRIPTION,
-	BASE_COST
-
-}
+CREATE TABLE immunizations (
+	DATE timestamp NOT NULL,
+	PATIENT_ID uuid NOT NULL REFERENCES patients(id),
+	ENCOUNTER_ID uuid NOT NULL REFERENCES encounters(id),
+	SNOMED_CODE text,
+	DESCRIPTION text,
+	BASE_COST money
+)
 --s
 --s2019-02-17T05:07:38Z,
 --sb9c610cd-28a6-4636-ccb6-c7a0d2a4cb85,
