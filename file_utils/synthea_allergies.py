dcd6ee3c3474b810e0716c860baaf1ac46d9b2ba
Jason Woods <jason@coder.haus>
2024-06-07 17:22:12 -0500

    Add python script to clean a synthea allergies file. - This will need to be massaged for the actual data in the file!

---
+++ file_utils/synthea_allergies.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+allergies_csv = "csv/allergies.csv"
+
+df = pd.read_csv(allergies_csv)
+
+# Just put a blank where there's no value. This will import as such, but we can be OK for testing.
+df.fillna('', inplace=True)
+
+# Not ideal, this is brute force. But for our testing, this should be fine
+df['SNOMED'] = ("( " + df[['REACTION1', 'DESCRIPTION1', 'SEVERITY1']]
+   .astype(str)
+   .agg(', '.join, axis=1).astype(str) + "), " +
+   "( "  + df[['REACTION2', 'DESCRIPTION2', 'SEVERITY2']]
+   .astype(str)
+   .agg(', '.join, axis=1).astype(str) + ")" )
+
+# Get the old columns out of here
+df.drop(columns = ['REACTION1', 'DESCRIPTION2', 'SEVERITY1', 'REACTION2', 'DESCRIPTION2', 'SEVERITY2'], inplace=True)
+
+df.to_csv("csv/allergies_cleaned.csv", index=False)
