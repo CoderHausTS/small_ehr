7e13f13796d51ec5855033829713b6dae6fd8dfc
Jason Woods <jason@coder.haus>
2024-06-09 21:06:55 -0500

    Start patient import functionality. - Add models for patient and newpatient

--- src/models.rs
+++ src/models.rs
@@ -22,7 +22,7 @@ pub struct Allergy {
     pub snomed: Option<Vec<Option<String>>>    // Vec<String>,
 }
 
-#[derive(Queryable, Selectable, Deserialize, Debug)]
+#[derive(Insertable, Selectable, Deserialize, Debug)]
 #[diesel(table_name = crate::schema::allergies)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(belongs_to(patient))]
@@ -38,4 +38,65 @@ pub struct NewAllergy {
     pub type_ : Option<String>,
     pub category: Option<String>,
     pub snomed: Option<Vec<Option<String>>>    // Vec<String>,
+}
+
+#[derive(Queryable, Selectable, Deserialize, Debug)]
+#[diesel(table_name = crate::schema::patients)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Patient {
+    pub id: Uuid,
+    pub bithdate: NaiveDate,
+    pub deathdate: Option<NaiveDate>,
+    pub ssn: Option<String>,
+    pub drivers: Option<String>,
+    pub passport: Option<String>,
+    pub prefix: Option<String>,
+    pub first: Option<String>,
+    pub last: Option<String>,
+    pub suffix: Option<String>,
+    pub maiden: Option<String>,
+    pub marital: Option<String>,
+    pub race: Option<String>,
+    pub ethnicity: Option<String>,
+    pub gender: Option<String>,
+    pub birthplace: Option<String>,
+    pub address: Option<String>,
+    pub city: Option<String>,
+    pub state: Option<String>,
+    pub county: Option<String>,
+    pub zip: Option<String>,
+    pub lat: Option<String>,
+    pub lon: Option<String>,
+    pub healthcare_expenses: Option<String>,
+    pub healthcare_coverage: Option<String>,
+}
+
+#[derive(Insertable, Selectable, Deserialize, Debug)]
+#[diesel(table_name = crate::schema::patients)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewPatient {
+    pub bithdate: NaiveDate,
+    pub deathdate: Option<NaiveDate>,
+    pub ssn: Option<String>,
+    pub drivers: Option<String>,
+    pub passport: Option<String>,
+    pub prefix: Option<String>,
+    pub first: Option<String>,
+    pub last: Option<String>,
+    pub suffix: Option<String>,
+    pub maiden: Option<String>,
+    pub marital: Option<String>,
+    pub race: Option<String>,
+    pub ethnicity: Option<String>,
+    pub gender: Option<String>,
+    pub birthplace: Option<String>,
+    pub address: Option<String>,
+    pub city: Option<String>,
+    pub state: Option<String>,
+    pub county: Option<String>,
+    pub zip: Option<String>,
+    pub lat: Option<String>,
+    pub lon: Option<String>,
+    pub healthcare_expenses: Option<String>,
+    pub healthcare_coverage: Option<String>,
 }
